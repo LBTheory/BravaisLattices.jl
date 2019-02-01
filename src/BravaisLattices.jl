10c8e9f3eb7c82d80aadd5f2853b16e53c195b33
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2019-02-01 14:38:16 -0200

    ADD the Manifest. Incipient work on the package

--- src/BravaisLattices.jl
+++ src/BravaisLattices.jl
@@ -1,5 +1,46 @@
+#------------------------------------------------------------------------------#
+#                                   Package                                    #
+#------------------------------------------------------------------------------#
+
+"""
+# 1. Description
+
+    module BravaisLattices
+
+Bravais (regular) lattices in  one  to  three  Euclidean  space  dimensions  for
+Theoretical Lattice Boltzmann.
+
+This package aims at providing tools for describing, storing,  and  manipulating
+Bravais (regular) lattices in one to three Euclidean space dimensions.
+
+# 2. Usage
+
+```julia-repl
+julia> import BravaisLattices
+
+```
+
+# 3. References
+
+"""
 module BravaisLattices
 
-greet() = print("Hello World!")
+
+#------------------------------------------------------------------------------#
+#                                   Imports                                    #
+#------------------------------------------------------------------------------#
+
+using Reexport
+
+
+#------------------------------------------------------------------------------#
+#                                   Includes                                   #
+#------------------------------------------------------------------------------#
+
+include("version.jl")
+
+include("BL.jl")
+@reexport using .BL
+
 
 end # module
